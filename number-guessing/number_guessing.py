686246d9433784fd3a54f238e6c820cf771c03cb
Yusuf Can DAL <dalyusufcan47@gmail.com>
2026-09-20 11:37:46 +0300

    number-guessing: add replay functionality

--- number-guessing/number_guessing.py
+++ number-guessing/number_guessing.py
@@ -1,31 +1,42 @@
 import random
 
-secret = random.randint(1,100)
-guess_counter = 0
-
 print("1-100 arasında sayı tahmin etme oyunu: ")
 
 while True:
-    try:
-        guess = int(input("Tahmininizi giriniz: "))
-    except ValueError: 
-        print("Lütfen sayı giriniz")
-        continue
+    secret = random.randint(1,100)
+    guess_counter = 0
+    
+    while True:
+        try:
+            guess = int(input("Tahmininizi giriniz: "))
+        except ValueError: 
+            print("Lütfen sayı giriniz")
+            continue
 
-    if guess < 1 or guess > 100:
-        print("Lütfen geçerli bir değer giriniz.")
-        continue
+        if guess < 1 or guess > 100:
+            print("Lütfen geçerli bir değer giriniz.")
+            continue
 
-    guess_counter += 1
+        guess_counter += 1
 
-    if guess < secret:
-        print("tahmininizi arttırın")
-        
+        if guess < secret:
+            print("tahmininizi arttırın")
+            
 
-    elif guess > secret:
-        print("tahmininizi küçültün")
-        
-    else:
-        
-        print(f"Tebrikler Tahmininiz Doğru \nGizli Sayı = {secret}\nAdım sayısı = {guess_counter}")
+        elif guess > secret:
+            print("tahmininizi küçültün")
+            
+        else:
+            
+            print(f"Tebrikler Tahmininiz Doğru \nGizli Sayı = {secret}\nAdım sayısı = {guess_counter}")
+            break
+
+    while True:
+        cevap = input("Tekrar oynamak ister misiniz? (e/h): ").lower()
+
+        if cevap == "e" or cevap == "h":
+            break
+
+        print("Lütfen sadece e veya h giriniz.")
+    if cevap == "h": 
         break
