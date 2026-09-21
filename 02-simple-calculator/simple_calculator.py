56cc5cf3fb983a85246dfd2ed0b8c92a52d7af9e
Yusuf Can DAL <dalyusufcan47@gmail.com>
2026-09-21 15:55:03 +0300

    Add files via upload

---
+++ 02-simple-calculator/simple_calculator.py
@@ -0,0 +1,39 @@
+while True:
+    try:
+        first_number = float(input("hesaplamak istediğiniz ilk sayıyı giriniz: "))
+    except ValueError:
+        print("Lütfen sayı giriniz. ")
+        continue
+    break
+
+while True:
+    operator = input("işlem seçiniz:")
+    if operator not in ("+", "-", "*", "/"):
+        print("Lütfen geçerli işlem giriniz")
+        continue
+    break
+
+while True:
+    try:
+        second_number = float(input("ikinci sayıyı giriniz:"))
+    except ValueError:
+        print("Lütfen sayı giriniz. ")
+        continue
+    if operator == "/" and second_number == 0:
+        print("Lütfen başka bir sayı giriniz")
+        continue
+    break
+
+if operator == "+":
+    result = first_number + second_number
+
+elif operator == "-":
+    result = first_number - second_number
+
+elif operator == "*":
+    result = first_number * second_number
+
+elif operator == "/":
+    result = first_number / second_number
+
+print(f"sonuç: {result}")
