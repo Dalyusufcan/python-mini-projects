8e1fcef4fe4507ff6830cd9f65aab006721c50cb
Yusuf Can DAL <dalyusufcan47@gmail.com>
2026-09-21 16:08:21 +0300

    Add files via upload

--- 02-simple-calculator/simple_calculator.py
+++ 02-simple-calculator/simple_calculator.py
@@ -1,39 +1,50 @@
-while True:
-    try:
-        first_number = float(input("hesaplamak istediğiniz ilk sayıyı giriniz: "))
-    except ValueError:
-        print("Lütfen sayı giriniz. ")
-        continue
-    break
+while True:   
+    while True:
+        try:
+            first_number = float(input("hesaplamak istediğiniz ilk sayıyı giriniz: "))
+        except ValueError:
+            print("Lütfen sayı giriniz. ")
+            continue
+        break
 
-while True:
-    operator = input("işlem seçiniz:")
-    if operator not in ("+", "-", "*", "/"):
-        print("Lütfen geçerli işlem giriniz")
-        continue
-    break
+    while True:
+        operator = input("işlem seçiniz:")
+        if operator not in ("+", "-", "*", "/"):
+            print("Lütfen geçerli işlem giriniz")
+            continue
+        break
 
-while True:
-    try:
-        second_number = float(input("ikinci sayıyı giriniz:"))
-    except ValueError:
-        print("Lütfen sayı giriniz. ")
-        continue
-    if operator == "/" and second_number == 0:
-        print("Lütfen başka bir sayı giriniz")
-        continue
-    break
+    while True:
+        try:
+            second_number = float(input("ikinci sayıyı giriniz:"))
+        except ValueError:
+            print("Lütfen sayı giriniz. ")
+            continue
+        if operator == "/" and second_number == 0:
+            print("Lütfen başka bir sayı giriniz")
+            continue
+        break
 
-if operator == "+":
-    result = first_number + second_number
+    if operator == "+":
+        result = first_number + second_number
 
-elif operator == "-":
-    result = first_number - second_number
+    elif operator == "-":
+        result = first_number - second_number
 
-elif operator == "*":
-    result = first_number * second_number
+    elif operator == "*":
+        result = first_number * second_number
 
-elif operator == "/":
-    result = first_number / second_number
+    elif operator == "/":
+        result = first_number / second_number
+    
+    print(f"sonuç: {result}")
 
-print(f"sonuç: {result}")
+    while True:
+        replay = input("tekrar hesaplamak ister misiniz(e/h)").lower()
+        if replay == "e" or replay == "h":
+            break
+        print("Lütfen sadece e veya h giriniz.")
+
+    if replay == "h": 
+        break
+            
