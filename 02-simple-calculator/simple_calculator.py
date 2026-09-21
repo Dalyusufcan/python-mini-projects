9c16fd928c3b8056368da6994d6ae6a8ed6413a3
Yusuf Can DAL <dalyusufcan47@gmail.com>
2026-09-21 16:31:30 +0300

    simple-calculator: refactor into functions
    02 - Completed MVP

--- 02-simple-calculator/simple_calculator.py
+++ 02-simple-calculator/simple_calculator.py
@@ -1,30 +1,25 @@
-while True:   
+def get_number(prompt):
     while True:
         try:
-            first_number = float(input("hesaplamak istediğiniz ilk sayıyı giriniz: "))
+            number = float(input(prompt))
         except ValueError:
             print("Lütfen sayı giriniz. ")
             continue
-        break
 
-    while True:
-        operator = input("işlem seçiniz:")
-        if operator not in ("+", "-", "*", "/"):
-            print("Lütfen geçerli işlem giriniz")
-            continue
-        break
+        return number
 
-    while True:
-        try:
-            second_number = float(input("ikinci sayıyı giriniz:"))
-        except ValueError:
-            print("Lütfen sayı giriniz. ")
-            continue
-        if operator == "/" and second_number == 0:
-            print("Lütfen başka bir sayı giriniz")
-            continue
-        break
 
+def get_operator():
+        while True:
+            operator = input("işlem seçiniz:")
+            if operator not in ("+", "-", "*", "/"):
+                print("Lütfen geçerli işlem giriniz")
+                continue
+
+            return operator
+
+
+def calculate(first_number, operator, second_number):
     if operator == "+":
         result = first_number + second_number
 
@@ -36,15 +31,40 @@ while True:
 
     elif operator == "/":
         result = first_number / second_number
-    
-    print(f"sonuç: {result}")
 
+    return result
+
+
+def ask_replay():
     while True:
         replay = input("tekrar hesaplamak ister misiniz(e/h)").lower()
         if replay == "e" or replay == "h":
-            break
+            return replay
         print("Lütfen sadece e veya h giriniz.")
 
-    if replay == "h": 
-        break
-            
+
+def main():
+    while True:
+        first_number = get_number("Hesaplamak istediğiniz ilk sayıyı giriniz: ")
+        operator = get_operator()
+
+        while True:
+            second_number = get_number("İkinci sayıyı giriniz: ")
+
+            if operator == "/" and second_number == 0:
+                print("Sıfıra bölme işlemi yapılamaz.")
+                continue
+
+            break
+
+        result = calculate(first_number, operator, second_number)
+        print(f"Sonuç: {result}")
+
+        replay = ask_replay()
+
+        if replay == "h":
+            break
+
+
+if __name__ == "__main__":
+    main()
